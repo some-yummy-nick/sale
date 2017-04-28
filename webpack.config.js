4600725b1996228c55cab5cc5eac9b401351464b
yeffasol@mail.ru <SldAP66nmd>
2017-04-28 08:57:45 +0300

    Add working site

--- webpack.config.js
+++ webpack.config.js
@@ -89,40 +89,7 @@ module.exports = {
       disable: false,
       allChunks: true
     }),
-    new FaviconsWebpackPlugin({
-      logo: './src/images/logo.png',
-      // The prefix for all image files (might be a folder or a name)
-      prefix: 'favicon/',
-      // Emit all stats of the generated icons
-      emitStats: false,
-      // The name of the json containing all favicon information
-      statsFilename: 'manifest.json',
-      // Generate a cache file with control hashes and
-      // don't rebuild the favicons until those hashes change
-      persistentCache: true,
-      // Inject the html into the html-webpack-plugin
-      inject: true,
-      // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
-      background: '#000',
-      // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-      title: 'Webpack App',
-
-      // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
-      icons: {
-        android: true,
-        appleIcon: true,
-        appleStartup: true,
-        coast: false,
-        favicons: true,
-        firefox: true,
-        opengraph: false,
-        twitter: false,
-        yandex: false,
-        windows: false
-      }
-    }),
     new CopyWebpackPlugin([
-      {from: 'src/fonts/', to: 'fonts'},
       {from: 'src/libraries/', to: 'libraries'}
     ])
   ]
